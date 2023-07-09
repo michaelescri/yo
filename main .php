47a5194dddb211d79bcc92101212937e0829b206
michaelescri <139026457+michaelescri@users.noreply.github.com>
2023-07-08 22:03:03 -0400

    Add files via upload

---
+++ main .php
@@ -0,0 +1,16 @@
+<?php
+  // Recibe los datos del formulario
+  $nombre = $_POST['nombre'];
+  $email = $_POST['email'];
+  $telefono = $_POST['telefono'];
+  $mensaje = $_POST['mensaje'];
+  // Configura los datos del correo electrónico
+          $destinatario = 'info.joyeriaemeie@gmail.com'; //Aquí debes sustituir tu@email.com por tu email
+  $asunto = 'Nuevo mensaje de formulario de contacto';
+  $cuerpo = "Nombre: $nombre\nEmail: $email\nTeléfono: $telefono\nMensaje: $mensaje";
+  $headers = "From: $email\r\nReply-To: $email\r\n";
+  // Envía el correo electrónico
+  mail($destinatario, $asunto, $cuerpo, $headers);
+  // Redirige al usuario a una página de confirmación
+  header('https://cursodepanaderiaypizzeria.blogspot.com/'); // confirmacion.html debe existir
+?>
